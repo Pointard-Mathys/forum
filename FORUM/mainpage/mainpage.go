7fc0e42abfa69fa761f739e56f4cbae3d2c4ab62
CCOINDARDLAGRESLE <fatalis@DESKTOP-HVPFHS8.localdomain>
2022-06-03 15:18:24 +0200

    réarrangement de l'architecture des fichiers + ajout de la fonction d'envoi de messages sur le serveur

--- FORUM/mainpage/mainpage.go
+++ FORUM/mainpage/mainpage.go
@@ -7,7 +7,7 @@ import (
 
 func Mainpage() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl, _ := template.ParseFiles("./FORUM/mainpage/mainpage.html")
+		tmpl, _ := template.ParseFiles("././static/mainpage/mainpage.html")
 		tmpl.Execute(w, r)
 	}
 }
--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
-// connection "forum/FORUM/ACCOUNT/connection"
-// mainpage "forum/FORUM/mainpage"
 import (
+	// connection "forum/FORUM/ACCOUNT/connection"
 	database "forum/FORUM/DATABASE"
+	mainpage "forum/FORUM/mainpage"
+	"net/http"
 )
+
+func testPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/testpage/testpage.html")
+}
 
 func main() {
 	database.DataBase()
-	// fs := http.FileServer(http.Dir("FORUM/CSS/"))
+	fs := http.FileServer(http.Dir("static/"))
 
-	// http.HandleFunc("/", mainpage.Mainpage())
+	http.HandleFunc("/", mainpage.Mainpage())
+	http.HandleFunc("/test", testPage)
 	// http.HandleFunc("/connection", connection.ConnectionPage())
-	// // http.HandleFunc("/login", login())
-	// // http.HandleFunc("/create-account", createAccount())
-	// //------------------------------------------------------------------
-	// http.Handle("/static/", http.StripPrefix("/static/", fs))
-	// http.ListenAndServe(":8080", nil)
+	// http.HandleFunc("/login", login())
+	// http.HandleFunc("/create-account", createAccount())
+	//------------------------------------------------------------------
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":8080", nil)
 }
