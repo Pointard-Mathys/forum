11c64ed05d502b028da5b415310f21aedc52763b
LCOURNOLLETRODRIGUEZ <louis.cournolletrodriguez@ynov.com>
2022-06-03 15:18:53 +0200

    commit to pull

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
+// connection "forum/FORUM/ACCOUNT/connection"
+// mainpage "forum/FORUM/mainpage"
 import (
-	connection "forum/FORUM/ACCOUNT/connection"
-	mainpage "forum/FORUM/mainpage"
-	"net/http"
+	database "forum/FORUM/DATABASE"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("FORUM/CSS/"))
+	database.DataBase()
+	// fs := http.FileServer(http.Dir("FORUM/CSS/"))
 
-	http.HandleFunc("/", mainpage.Mainpage())
-	http.HandleFunc("/connection", connection.ConnectionPage())
-	// http.HandleFunc("/login", login())
-	// http.HandleFunc("/create-account", createAccount())
-	//------------------------------------------------------------------
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.ListenAndServe(":8080", nil)
+	// http.HandleFunc("/", mainpage.Mainpage())
+	// http.HandleFunc("/connection", connection.ConnectionPage())
+	// // http.HandleFunc("/login", login())
+	// // http.HandleFunc("/create-account", createAccount())
+	// //------------------------------------------------------------------
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+	// http.ListenAndServe(":8080", nil)
 }
