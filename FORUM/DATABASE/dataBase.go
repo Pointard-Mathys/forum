0f61d5b956134b60f98f9e8f5de20a8b25eab465
MPOINTARD <mathys.pointard@ynov.com>
2022-06-13 16:23:19 +0200

    archie affiche que 0 fini

--- FORUM/DATABASE/dataBase.go
+++ FORUM/DATABASE/dataBase.go
@@ -22,6 +22,7 @@ type Topic struct {
 	contain    string
 	nombre_rep int
 	id_user    int
+	archive    int
 }
 
 type Reponse struct {
@@ -29,6 +30,12 @@ type Reponse struct {
 	TextContent string
 	Id_user     int
 	Id_topic    int
+}
+
+type like struct {
+	Id         int
+	Id_user    int
+	Id_reponse int
 }
 
 func InitDatabase(database string) *sql.DB {
@@ -52,16 +59,24 @@ func InitDatabase(database string) *sql.DB {
 		contain TEXT NOT NULL,
 		nombre_rep INTEGER,
 		id_user INTEGER NOT NULL,
+		archive INEGER NOT NULL,
 		FOREIGN KEY(id_user) REFERENCES users(id)
 		);
 
-	CREATE TABLE IF NOT EXISTS reponse (
+	CREATE TABLE IF NOT EXISTS reponses (
 		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		content TEXT NOT NULL,
-		id_user TEXT NOT NULL,
-		id_topic INTEGER,
+		id_topic INTEGER NOT NULL,
+		id_user INTEGER NOT NULL,
 		FOREIGN KEY(id_user) REFERENCES users(id)
 		FOREIGN KEY(id_topic) REFERENCES topics(id)
+	);
+	CREATE TABLE IF NOT EXISTS likes (
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		id_reponse INTEGER,
+		id_user TEXT NOT NULL,
+		FOREIGN KEY(id_user) REFERENCES users(id)
+		FOREIGN KEY(id_reponse) REFERENCES reponses(id)
 	)
 	`
 	_, err = db.Exec(sqlStmt)
@@ -87,8 +102,8 @@ func InsertIntoUsers(db *sql.DB, name string, email string, password string) (in
 	return result.LastInsertId()
 }
 
-func InsertIntoTopic(db *sql.DB, titre string, contain string, nombre_rep int, id_user int) (int64, error) {
-	result, err := db.Exec(`INSERT INTO topics (titre, contain, nombre_rep, id_user) VALUES (?, ?, ?, ?)`, titre, contain, nombre_rep, id_user)
+func InsertIntoTopic(db *sql.DB, titre string, contain string, nombre_rep int, id_user int, archive int) (int64, error) {
+	result, err := db.Exec(`INSERT INTO topics (titre, contain, nombre_rep, id_user, archive) VALUES (?, ?, ?, ?, ?)`, titre, contain, nombre_rep, id_user, archive)
 	if err != nil {
 		log.Printf("ERR: %s\n", err)
 		return -1, nil
@@ -96,8 +111,17 @@ func InsertIntoTopic(db *sql.DB, titre string, contain string, nombre_rep int, i
 	return result.LastInsertId()
 }
 
-func InsertIntoReponse(db *sql.DB, content string, id_user int, id_topic int) (int64, error) {
-	result, err := db.Exec(`INSERT INTO reponse (content, id_user, id_topic) VALUES (?, ?, ?)`, content, id_user, id_topic)
+func InsertIntoReponse(db *sql.DB, content string, id_topic int, id_user int) (int64, error) {
+	result, err := db.Exec(`INSERT INTO reponses (content, id_topic, id_user) VALUES (?, ?, ?)`, content, id_topic, id_user)
+	if err != nil {
+		log.Printf("ERR: %s\n", err)
+		return -1, nil
+	}
+	return result.LastInsertId()
+}
+
+func InsertIntoLike(db *sql.DB, id_reponse int, id_user int) (int64, error) {
+	result, err := db.Exec(`INSERT INTO likes (id_reponse, id_user) VALUES (?, ?)`, id_reponse, id_user)
 	if err != nil {
 		log.Printf("ERR: %s\n", err)
 		return -1, nil
@@ -124,7 +148,7 @@ func DisplayUserRows(rows *sql.Rows) {
 func DisplayTopicRows(rows *sql.Rows) {
 	for rows.Next() {
 		var topic Topic
-		err := rows.Scan(&topic.Id, &topic.Titre, &topic.contain, &topic.nombre_rep, &topic.id_user)
+		err := rows.Scan(&topic.Id, &topic.Titre, &topic.contain, &topic.nombre_rep, &topic.id_user, &topic.archive)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -148,6 +172,48 @@ func DeleteUsersById(db *sql.DB, table string, id int) {
 	db.Exec("DELETE FROM "+table+" WHERE id = ?", id)
 }
 
+func UpDate(db *sql.DB, option string, jacklyne string, new string, id int) {
+	if jacklyne != "id" {
+		db.Exec("UPDATE "+option+" SET "+jacklyne+" = "+"'"+new+"'"+" WHERE id = ?;", id)
+	}
+}
+
+func Count(db *sql.DB, table string, id_reponse int) int {
+	var n int
+	result := db.QueryRow("SELECT COUNT(*) FROM "+table+" WHERE id_reponse = ?", id_reponse)
+	result.Scan(&n)
+	return n
+}
+
+// func SelectArchiveFromTopic(db *sql.DB, archive int) *sql.Rows {
+// 	var topic Topic
+// 	pattern := db.QueryRow("SELECT * FROM topics WHERE archive =?", archive)
+// 	pattern.Scan(&topic.Id, &topic.Titre, &topic.contain, &topic.nombre_rep, &topic.id_user, &topic.archive)
+// 	result, _ := db.Query(pattern)
+// 	return result
+// }
+
+func SelectArchiveFromTopic(db *sql.DB, archive int) ([]Topic, error) {
+	pattern, err := db.Query("SELECT * FROM topics WHERE archive =?", archive)
+	if err != nil {
+		log.Printf("ERR: %s\n", err)
+		return nil, err
+	}
+
+	var topics []Topic
+	for pattern.Next() {
+		var topic Topic
+		if err := pattern.Scan(&topic.Id, &topic.Titre, &topic.contain, &topic.nombre_rep, &topic.id_user, &topic.archive); err != nil {
+			return topics, err
+		}
+		topics = append(topics, topic)
+	}
+	if err = pattern.Err(); err != nil {
+		return topics, err
+	}
+	return topics, nil
+}
+
 func DataBase() {
 	db := InitDatabase("FORUM/DATABASE/databaseHolder/DATA_BASE.db")
 	defer db.Close()
@@ -158,8 +224,19 @@ func DataBase() {
 	InsertIntoUsers(db, "com", "vane@gmail.com", Encoding_password("test"))
 	InsertIntoUsers(db, "com", "srvvane@gmail.com", Encoding_password("test"))
 
-	// fmt.Println("\n")
-	// fmt.Println("dataBase")
+	InsertIntoTopic(db, "test", "j fait un test", 0, 4, 1)
+
+	InsertIntoReponse(db, "je rep", 1, 6)
+
+	InsertIntoLike(db, 2, 2)
+	InsertIntoLike(db, 2, 2)
+	InsertIntoLike(db, 2, 2)
+
+	fmt.Println(Count(db, "likes", 1))
+
+	// topic := SelectArchiveFromTopic(db, 0)
+	// DisplayTopicRows(topic)
+	fmt.Println(SelectArchiveFromTopic(db, 1))
 	// rows := SelectAllFromTable(db, "users")
 	// DisplayUserRows(rows)
 
@@ -175,9 +252,8 @@ func DataBase() {
 
 	// DeleteUsersById(db, "topics", 1)
 
-	fmt.Println("\n")
-	fmt.Println("topics")
-	InsertIntoTopic(db, "test", "j fait un test", 0, 4)
-	topic := SelectAllFromTable(db, "topics")
-	DisplayTopicRows(topic)
+	// UpDate(db, "users", "email", "nazi.super.MILF@gmail.con", 1)
+
+	// fmt.Println("\n")
+	// fmt.Println("topics")
 }
