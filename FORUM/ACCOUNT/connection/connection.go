a67500357fbdf41de9fe317bd4864ff65d6529fb
CCOINDARDLAGRESLE <fatalis@DESKTOP-HVPFHS8.localdomain>
2022-06-19 01:42:04 +0200

    rebase

--- FORUM/ACCOUNT/connection/connection.go
+++ FORUM/ACCOUNT/connection/connection.go
@@ -1,13 +1,9 @@
 package forum
 
 import (
-	"html/template"
 	"net/http"
 )
 
-func ConnectionPage() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl, _ := template.ParseFiles("./FORUM/ACCOUNT/connection/connection.html")
-		tmpl.Execute(w, r)
-	}
+func ConnectionPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "././static/ACCOUNT/connection/connection.html")
 }
